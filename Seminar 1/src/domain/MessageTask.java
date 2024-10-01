5c8e7126417eab36afcbef77c04300d8b06f729c
Horaţiu <42303594+H0R4T1U@users.noreply.github.com>
2024-10-01 20:36:28 +0300

    Add files via upload

---
+++ Seminar 1/src/domain/MessageTask.java
@@ -0,0 +1,36 @@
+package domain;
+
+import Utils.Util;
+
+import java.time.LocalDateTime;
+
+public class MessageTask extends Task {
+    private String message;
+    private String from;
+    private String to;
+    LocalDateTime date;
+
+    public MessageTask(String id, String desc,String message, String from, String to, LocalDateTime date) {
+        super(id, desc);
+        this.message = message;
+        this.from = from;
+        this.to = to;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+
+        return super.toString() + "\n MessageTask{" +
+                "message='" + message + '\'' +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("MessageTask executed! "+ date.format(Util.formatter) + "\n");
+    }
+}
