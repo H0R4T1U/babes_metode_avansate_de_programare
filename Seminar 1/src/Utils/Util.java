2cb03349791abe1ed8ff6385bce4aad6797a4da7
Horaţiu <horatiubanciu11@gmail.com>
2024-10-01 20:36:28 +0300

    Add files via upload

---
+++ Seminar 1/src/Utils/Util.java
@@ -0,0 +1,7 @@
+package Utils;
+
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+}
